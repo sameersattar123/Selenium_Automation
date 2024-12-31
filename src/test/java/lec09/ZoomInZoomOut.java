e8f6a466153ba89870eaf6e29391f9212e549a7e
Sameer <sameersattar1111@gmail.com>
2024-12-31 07:16:37 +0500

    Zoom In zoom out practice

---
+++ src/test/java/lec09/ZoomInZoomOut.java
@@ -0,0 +1,31 @@
+package lec09;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ZoomInZoomOut {
+    public static void main(String[] args) throws InterruptedException {
+         WebDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+        Thread.sleep(5000);
+
+        driver.manage().window().maximize();
+
+        Thread.sleep(5000);
+
+        driver.manage().window().minimize();
+
+        Thread.sleep(5000);
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+
+        js.executeScript("document.body.style.zoom = '50%';");
+
+        Thread.sleep(5000);
+
+        js.executeScript("document.body.style.zoom = '100%';");
+
+    }
+}
